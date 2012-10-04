7d1e7c6d560092a8b9dd7fc59036b421add9a3f6
Stéphan Kochen <stephan@angrybytes.com>
2012-10-04 13:43:32 +0200

    Failing tests for return values.

--- test.js
+++ test.js
@@ -18,59 +18,59 @@ function expectEvent(t, obj, name, block) {
 }
 
 test('resetModel', function(t) {
-    t.plan(4);
+    t.plan(7);
 
     var model = new Backbone.Model();
 
     expectEvent(t, model, 'change:foo', function() {
-        model.reset({ foo: 5 });
+        t.ok(model.reset({ foo: 5 }), "add first attribute");
     });
 
     expectEvent(t, model, 'change:bar', function() {
-        model.reset({ foo: 5, bar: 8 });
+        t.ok(model.reset({ foo: 5, bar: 8 }), "add second attribute");
     });
 
     expectEvent(t, model, 'change:foo', function() {
-        model.reset({ bar: 8 });
+        t.ok(model.reset({ bar: 8 }), "remove first attribute");
     });
 
     t.notOk(_.has(model.attributes, 'foo'), 'attribute should be deleted');
 });
 
 test('collectionDelta', function(t) {
-    t.plan(5);
+    t.plan(10);
 
     var collection = new Backbone.Collection();
 
     expectEvent(t, collection, 'reset', function() {
-        collection.delta([
+        t.ok(collection.delta([
             { id: 5 }
-        ]);
+        ]), "add first model");
     });
 
     expectEvent(t, collection, 'add', function() {
-        collection.delta([
+        t.ok(collection.delta([
             { id: 5 },
             { id: 8 }
-        ]);
+        ]), "add second model");
     });
 
     expectEvent(t, collection, 'change', function() {
-        collection.delta([
+        t.ok(collection.delta([
             { id: 5, foo: 'bar' },
             { id: 8 }
-        ]);
+        ]), "change first model");
     });
 
     expectEvent(t, collection, 'remove', function() {
-        collection.delta([
+        t.ok(collection.delta([
             { id: 8 }
-        ]);
+        ]), "remove first model");
     });
 
     expectEvent(t, collection, 'reset', function() {
-        collection.delta([
+        t.ok(collection.delta([
             { id: 3 }
-        ]);
+        ]), "replace contents");
     });
 });
